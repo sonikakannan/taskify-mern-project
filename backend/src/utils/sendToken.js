18522222a6c1c9c5ae07f54283a5e0211b7f606c
sonikakannan <sonikakannan66@gmail.com>
2025-05-19 17:57:49 +0530

    first commit

--- backend/src/utils/sendToken.js
+++ backend/src/utils/sendToken.js
@@ -17,8 +17,8 @@ export const sendToken = (user, statusCode, message, res) => {
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax",
+        secure: process.env.NODE_ENV === "production", 
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     })
     .json({
       success: true,
